6379e3dc442372ee46bf48efc68c42f641941f5e
Andrii Taranenko <megathrone3333@gmail.com>
2024-05-11 10:53:09 +0200

    Init develop

--- resource/src/app/page.tsx
+++ resource/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Production environment
+          Development environment
         </p>
         <div>
           <a
